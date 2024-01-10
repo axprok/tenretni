47039a91489d2e23f8e4df4aef5da25b8db1b2b8
axprok <axprok@wm.edu>
2024-01-10 14:38:09 +0100

    update

---
+++ pages/courseSyllabi/rusn381/main.js
@@ -0,0 +1,6 @@
+
+
+// <button type="button" onclick="document.getElementById('demo').innerHTML = Date()">
+// Click me to display Date and Time.</button>
+<button type="button" onclick='document.getElementById("test1").innerHTML = "Rusn 381. Decolonizing Putins Russia"'>Click Me!</button>
+
